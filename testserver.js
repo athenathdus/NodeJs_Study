e6b40d7473a0f8c64192afe2006ae4bba162e081
athenathdus <athenathdus@gmail.com>
2021-08-11 08:30:07 +0000

    first commit

---
+++ testserver.js
@@ -0,0 +1,11 @@
+// console.log('hellojs');
+
+const http = require('http');
+
+const http_server = http.createServer(function (req, res) {
+    console.log(req.url);
+    res.end('hello http server!!!!!');
+});
+
+http_server.listen(8080);
+console.log('start http server at 8080')
